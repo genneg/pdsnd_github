3cba187954ad63c2bda3eef7df3eab26a6db9d01
Gennaro Negri <gennaro.negri@pm.me>
2020-07-10 11:45:55 +0200

    docs: Add internal documentation

--- bikeshare.py
+++ bikeshare.py
@@ -56,9 +56,11 @@ def load_data(city, month, day):
     Loads data for the specified city and filters by month and day if applicable.
 
     Args:
-        (str) city - name of the city to analyze
+        (str) city - name of the city to analyze Chicago, New York City, or Washington
         (str) month - name of the month to filter by, or "all" to apply no month filter
+            January, February, March, April, May, or June
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
